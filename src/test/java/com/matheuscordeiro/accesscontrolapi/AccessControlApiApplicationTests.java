8021d9ecaea666d7f1208254a2554a1f46a8e4ac
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-07-15 21:49:38 -0300

    First commit

---
+++ src/test/java/com/matheuscordeiro/accesscontrolapi/AccessControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.accesscontrolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
